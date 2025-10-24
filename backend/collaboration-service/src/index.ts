41f0e1e24a40406369667aac3d2f7e926530790d
thony-ui <anthonyhermanto1@gmail.com>
2025-10-24 23:04:37 +0800

    convert to linux

--- backend/collaboration-service/src/index.ts
+++ backend/collaboration-service/src/index.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 // app.use(express.json({ limit: '512kb' }));
-
+// test deployment
 app.use("/sessions", sessionsRouter);
 
 const port = process.env.COLLAB_SERVICE_PORT || 6004;
